cfcbaba79be55c2c22440c168701ec4d4664e170
Brian-304 <User@Brian.com>
2022-10-18 09:36:18 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,10 @@
+// var sum = 0
+// arr = [3, 17, 10, 5, 15]
+
+// for(var i = 0; i < arr.length; i++) {
+// sum = sum + arr[i];
+//     }
+// avg = sum / arr.length
+// console.log(avg)
+ 
+
---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+
+@app.route('/')
+def index():
+    if 'counter' in session:
+        session['counter'] += 1
+    else:
+        session['counter'] = 1
+    return render_template("index.html")
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect("/")
+
+if __name__=="__main__":
+    app.run(debug=True)
